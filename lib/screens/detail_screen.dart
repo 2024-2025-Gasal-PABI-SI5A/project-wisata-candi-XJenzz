d3e95102ca48792ae5d4e67950d438e35a705234
Bryan Penuntun <cyberjrs.p@gmail.com>
2024-11-09 11:31:51 +0700

    finish detail detail

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -9,7 +9,51 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          //DETAIL HEADER
+          //IMAGE UTAMA
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              //TOMBOL BACK CUSTOM
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[100]?.withOpacity(0.8),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    onPressed: (){}, 
+                    icon: const Icon(Icons.arrow_back)
+                    ), 
+                ),
+              ),
+            ],
+          ),
+          //DETAIL INFO
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                //Info Atas
+                //Info Tengah
+                //Info Bawah
+              ],
+            ),
+            )
+        ],
       ),
     );
   }
